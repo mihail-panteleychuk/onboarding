3ff63681f786850be290de4e880b1805f30ed6e5
mihail-panteleychuk <mykhailo_p@dataforest.ai>
2026-01-20 12:34:49 +0200

    Initial commit

---
+++ web_app/apps/user/utils.py
@@ -0,0 +1,16 @@
+from django.contrib.gis.geoip2 import GeoIP2
+
+from apps.user.models import Country
+
+
+def detect_country_by_ip(request):
+    """
+    Detecting request country by customer IP
+    """
+    g = GeoIP2()
+    ip = request.META.get("HTTP_X_FORWARDED_FOR")
+    if not ip:
+        return None
+    country = g.country(ip.split(",")[0])
+    country = Country.objects.filter(name=country["country_name"]).first()
+    return country
